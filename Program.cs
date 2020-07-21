a5e8d206fb5b30ad6a978fe866cc1ba8e360b91d
AndreyVAAVA <62995060+AndreyVAAVA@users.noreply.github.com>
2020-07-22 02:34:14 +0300

    Upgrade

--- Program.cs
+++ Program.cs
@@ -6,19 +6,28 @@ namespace CodeBlog_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! ТЫ ПИДОР!");
-            Console.ReadLine();
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Wake the fuck up, Samurai");
-            Console.ReadLine();
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("We have a city to burn");
-            Console.ReadLine();
-            Console.Clear();
+            //string s = "1";
+            //int i = (string)s;
+            //int j = (byte)5;
+            //double d = j;
+            //int k = (double)d;
+            //int i = 5;
+            //int j = 2048;
+            //byte b = (byte)i;
+            //byte bb = (byte)j;
+            //Console.WriteLine(bb);
+
+            //string s = "2048";
+            //int jojo = Convert.ToInt32(s);
+            //int huis = Int32.Parse("42");
+            //Console.WriteLine(jojo);
+
+            int i = 1;
+            string s = "str" + i.ToString();
+
+            //byte b = 42;
+            //int o = b;
+
         }
     }
 }
