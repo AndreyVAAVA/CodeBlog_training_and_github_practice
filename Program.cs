5c1bc72062461baea63f9265db5109c00bd1c31e
AndreyVAAVA <62995060+AndreyVAAVA@users.noreply.github.com>
2020-07-21 03:09:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeBlog_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! ТЫ ПИДОР!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake the fuck up, Samurai");
+            Console.ReadLine();
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("We have a city to burn");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
