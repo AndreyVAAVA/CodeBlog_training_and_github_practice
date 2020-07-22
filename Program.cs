02c3dad01717e8e7f6e67dd1d1a71b2eb0f7eb3d
AndreyVAAVA <62995060+AndreyVAAVA@users.noreply.github.com>
2020-07-22 23:21:02 +0300

    Finished homework

--- Program.cs
+++ Program.cs
@@ -21,9 +21,45 @@ namespace CodeBlog_1
             list.Add(0);
             list.Add(1);
             list.Add(2);
+            list.AddRange(new int[] { 0, 1 });
+
+            C[] cs = new C[10];
+            List<C> css = new List<C>();
 
             Console.WriteLine(list[0]);
             Console.WriteLine(Days.Mon);
+            List<string> lst5 = new List<string>() 
+            {
+                "AMD", "TOP", "YOU", "UNDERSTAND", "?" 
+            };
+            int[] amd = new int[5] 
+            {
+                1, 2, 3, 4, 5
+            };
+            int ling = 0;
+            while (ling < 5) 
+            {
+                Console.WriteLine(lst5[ling]);
+                Console.WriteLine(amd[ling]);
+                ling++;
+            }
+            Console.WriteLine(lng5.first);
+            Console.WriteLine(lng5.second);
+            Console.WriteLine(lng5.third);
+            Console.WriteLine(lng5.fourth);
+            Console.WriteLine(lng5.fifth);
         }
+        public class C
+        {
+
+        }
+        enum lng5
+        {
+            first,
+            second,
+            third,
+            fourth,
+            fifth
+        };
     }
 }
