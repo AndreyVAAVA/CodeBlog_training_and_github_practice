d1c22009295fa221946cd84d92afec0173dc7ec4
AndreyVAAVA <62995060+AndreyVAAVA@users.noreply.github.com>
2020-07-28 03:21:07 +0300

    HomeWork

---
+++ NewFolder1/Consuming.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlog_1.NewFolder1
+{
+    public class Consuming<T>
+    where T : Component
+    {
+        public double TDP_Perf { get; private set; }
+        public double Perf_per_Watt(T component)
+        {
+            TDP_Perf = (component.PowerConsuming * component.SIZE) / component.Price;
+            return TDP_Perf;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeBlog_1.NewFolder1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,14 +10,17 @@ namespace CodeBlog_1
 
         static void Main(string[] args)
         {
-            var eating = new Eating<Apple>();
+            //var eating = new Eating<Apple>();
 
             /*var p = new Product<int>("Apple", 100, 100);
             var pp = new Product<decimal>("Banan", 10.1M, 11.1M);*/
 
-            var list = new List<int>();
+            /*var list = new List<int>();
             var map = new Dictionary<int, string>();
-            map.Add(5, "Fifth");
+            map.Add(5, "Fifth");*/
+
+            RAM ddr4 = new RAM("Ballistix", 500, 32, 15, 63.4M);
+            var perf_per_doll = new Consuming<RAM>();
         }
     }
 }
