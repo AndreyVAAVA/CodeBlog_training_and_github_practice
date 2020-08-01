ef9c41ec2b76211ef163607d0d34b1a2cf472a49
AndreyVAAVA <62995060+AndreyVAAVA@users.noreply.github.com>
2020-08-01 03:19:17 +0300

    everything

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeBlog_1
+{
+    class Person
+    {
+        public event Action GoToSleep;
+        public event EventHandler DoWork;
+        public string Name { get; set; }
+
+        public void TakeTime(DateTime now) 
+        {
+            if(now.Hour <= 8)
+            {
+                GoToSleep?.Invoke();
+            }
+            else
+            {
+                var args = new EventArgs();
+                DoWork?.Invoke(this, args);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,112 +4,105 @@ using System.Linq;
 
 namespace CodeBlog_1
 {
+    public delegate void MyDelegate();
     partial class Program
     {
-
+        public delegate int ValueDelegate(int i);
+        public event MyDelegate Event;
+        public event Action EventAction;
         static void Main(string[] args)
         {
-            /*int a = 200000000;
-            int b = 200000000;
-            int c = checked(a * b);*/
+            #region delegate
+            /*MyDelegate myDelegate = Method1;
+            myDelegate += Method4;
 
-            try
-            {
-                var i = 5;
-                var j = i / 0;
-                Console.WriteLine(j);
-            }
-            catch(DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("DivideByZeroException");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("Exception");
-                throw;
+            MyDelegate myDelegate2 = new MyDelegate(Method3);
+            myDelegate2 += Method4;
+            MyDelegate myDelegate3 = myDelegate + myDelegate2;
+            myDelegate();
+            myDelegate2.Invoke();
+            myDelegate3.Invoke();
 
-            }
-            finally
+            var valueDelegate = new ValueDelegate(MethodValue);
+            valueDelegate += MethodValue;
+            valueDelegate += MethodValue;
+            valueDelegate += MethodValue;
+            valueDelegate -= MethodValue;
+            valueDelegate((new Random()).Next(10,50));
+
+            Action action = Method1;
+            action();
+
+            Predicate<int> predicate;
+
+            Func<int, int> func = MethodValue;
+
+            func?.Invoke(7);*/
+            #endregion
+
+            #region event
+            /*Person person = new Person()
             {
-                Console.WriteLine("Application closed");
+                Name = "Vasya"
+            };
+            person.GoToSleep += Person_GoToSleep;
+            person.DoWork += Person_DoWork;
+            person.TakeTime(DateTime.Parse("27.12.2018 21:13:01"));
+            person.TakeTime(DateTime.Parse("27.12.2018 4:13:01"));*/
+            #endregion
+
+            Console.WriteLine(Sum(5, 5, Calc1));
+            Console.WriteLine(Sum(5, 5, Calc2));
+        }
+
+        private static void Person_DoWork(object sender, EventArgs e)
+        {
+            if(sender is Person)
+            {
+            Console.WriteLine($"{((Person)sender).Name} work");
             }
 
-            /*while (true)
-            {
-                var input = Console.ReadLine();
-                if (int.TryParse(input, out int result))
-                {
-                    Console.WriteLine(result);
-                }
-                else
-                {
-                    Console.WriteLine("Введите целое число");
-                }
-                break;
-            }
-            int i = 5;
-            try
-            {
-                throw new MyOwnException();
-            }
-            catch (MyOwnException ex)
-            {
-                Console.WriteLine("My Own Excpetion");
-            }
-            catch (DivideByZeroException ex) when (i == 5)
-            {
-                Console.WriteLine("Деление на 0 (i = 5)");
-                throw;
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("Деление на ноль");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Work finished");
-            }
-            finally
-            {
-                Console.WriteLine("Work ended");
-            }*/
-            var gaming = new PC();
-            gaming.Cooler = 0;
-            gaming.CPU = 0;
-            gaming.DriveDevice = 0;
-            gaming.GraphicsCard = 0;
-            gaming.Motherboard = 0;
-            gaming.PowerSupply = 0;
-            gaming.RAM = 0;
-            try
-            {
-                var cpu = 1 / gaming.CPU;
-                var cooler = 1 / gaming.Cooler;
-                var drivedevice = 1 / gaming.DriveDevice;
-                var graphicscard = 1 / gaming.DriveDevice;
-                throw new MyOwnException();
-            }
-            catch (MyOwnException ex)
-            {
-                Console.WriteLine("ЭЭЭЭ");
-                throw ex;
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine("AAAA");
-                throw ex;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("BBBB");
-                throw ex;
-            }
-            finally
-            {
-                Console.WriteLine("Ты дурак, как может быть 0 вещей, учитывая, что без этого комп не будет работать");
-            }
+        }
+
+        private static void Person_GoToSleep()
+        {
+            Console.WriteLine("Human GoToSleep");
+        }
+
+        public static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+        public static int Method2()
+        {
+            Console.WriteLine("Method2");
+            return 0;
+        }
+        public static void Method3()
+        {
+            Console.WriteLine("Method3");
+        }
+        public static void Method4()
+        {
+            Console.WriteLine("Method4");
+        }
+        public static int MethodValue(int i)
+        {
+            Console.WriteLine(i);
+            return i;
+        }
+        private static int Sum(int a, int b, Func<int, int, int> calc)
+        {
+            return calc(a, b);
+        }
+        private static int Calc1(int i, int j)
+        {
+            return i + j;
+        }
+        private static int Calc2(int i, int j)
+        {
+            return i * j;
         }
     }
+
 }
