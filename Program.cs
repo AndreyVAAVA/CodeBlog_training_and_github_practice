6bce3631fbd88bc9811e2ac03c7fbfadb4ad1830
AndreyVAAVA <62995060+AndreyVAAVA@users.noreply.github.com>
2020-08-05 14:23:06 +0300

    SCK

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,86 +13,39 @@ namespace CodeBlog_1
     public delegate void MyDelegate();
     class Program
     {
-        public static object locker = new object();
         static void Main(string[] args)
         {
-            #region async
-            /*var calc = new Perf_Calc();
-            var result = calc.PerfAsync();
-            Console.ReadLine();
-            Console.WriteLine(result.Result);*/
-            #endregion
-            #region thread
-            Thread thread = new Thread(Logo);
-            Thread thread2 = new Thread(Logo);
-            Thread thread3 = new Thread(Logo);
+            const string ip = "127.0.0.1";
+            const int port = 8080;
 
-            // Set the priority of threads 
-            thread.Priority = ThreadPriority.Highest;
-            thread3.Priority = ThreadPriority.BelowNormal;
-            thread3.Start();
-            thread.Start();
-            thread2.Start();
-            //thread.Abort();
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
-            // Display the priority of threads 
-            Console.WriteLine("The priority of thread is: {0}",
-                                              thread.Priority);
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-            Console.WriteLine("The priority of thread2 is: {0}",
-                                              thread2.Priority);
+            Console.WriteLine("Введите сообщение:");
+            var message = Console.ReadLine();
 
-            Console.WriteLine("The priority of thread3 is: {0}",
-                                              thread3.Priority);
-            Thread T = new Thread(new ParameterizedThreadStart(Logo2));
-            T.Start("Extreme");
-            // Creating and initializing threads 
-            /*Thread T1 = new Thread(work);
-            Thread T2 = new Thread(work);
-            Thread T3 = new Thread(work);
+            var data = Encoding.UTF8.GetBytes(message);
 
-            // Set the priority of threads 
-            T2.Priority = ThreadPriority.Highest;
-            T3.Priority = ThreadPriority.BelowNormal;
-            T1.Start();
-            T2.Start();
-            T3.Start();
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
 
-            // Display the priority of threads 
-            Console.WriteLine("The priority of T1 is: {0}",
-                                              T1.Priority);
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
 
-            Console.WriteLine("The priority of T2 is: {0}",
-                                              T2.Priority);
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
 
-            Console.WriteLine("The priority of T3 is: {0}",
-                                              T3.Priority);*/
+            Console.WriteLine(answer.ToString());
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
         }
-        /*public static void work()
-        {
-
-            // Sleep for 1 second 
-            Thread.Sleep(1000);
-        }*/
-        /*public static void work()
-        {
-
-            // Sleep for 1 second 
-            Thread.Sleep(1000);
-        }*/
-        public static void Logo()
-        {
-            Console.WriteLine( "Nvidia" + "AMD" + "Intel");
-            Thread.Sleep(1000);
-        }
-        public static void Logo2(object max)
-        {
-            Console.WriteLine($"{max} Nvidia AMD Intel");
-            Thread.Sleep(1000);
-        }
-
-
-        #endregion
     }
 
 }
